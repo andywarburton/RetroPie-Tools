9baa8eb92c9b1f95ce2c768d10dccbe3550c5710
Andy Warburton <andy@thewarburtons.co.uk>
2016-04-24 23:45:14 +0200

    made this safer for first time users
    changed yes/no option to test/clean/delete so the user is more implicit in their actions

--- cleanup.py
+++ cleanup.py
@@ -5,23 +5,31 @@ import string
 
 # config vars, if you are using non-standard stuff change these
 rom_dir         = '/home/pi/RetroPie/roms'
+bak_dir         = '/home/pi/RetoPie/cleaned_up'
 img_dir         = '/opt/retropie/configs/all/emulationstation/downloaded_images'
-allowed_systems = ['amiga','amstradcpc','apple2','arcade','atari800','atari2600','atari5200','atari7800','atarilynx','atarist','c64','coco','dragon32','dreamcast','fba','fds','gamegear','gb','gba','gbc','intellivision','macintosh','mame-advmame','mame-libretro','mame-mame4all','mastersystem','megadrive','msx','n64','neogeo','nes','ngp','ngpc','pc','pcengine','psp','psx','scummvm','sega32x','segacd','sg-1000','snes','vectrex','videopac','wonderswan','wonderswancolor','zmachine','zxspectrum']
+allowed_systems = ['amiga','amstradcpc','apple2','arcade','atari800','atari2600','atari5200','atari7800','atarilynx','atarist','c64','coco','dragon32','dreamcast','fba','fds','gamegear','gb','gba','gbc','intellivision','macintosh','mame-advmame','mame-libretro','mame-mame4all','mastersystem','megadrive','msx','n64','neogeo','nes','ngp','ngpc','pc','pcengine','psp','psx','sega32x','segacd','sg-1000','snes','vectrex','videopac','wonderswan','wonderswancolor','zmachine','zxspectrum']
 delete_count    = 0
 total_count     = 0
 
 question = """  
-*************************************************************
-    WARNING! THIS SCRIPT WILL PERMANENTLY DELETE FILES!
-        AND IS WRITTEN BY AN AMATEUR PYTHON DEVELOPER
+*****************************************************************
+      WARNING! THIS SCRIPT WILL PERMANENTLY DELETE FILES!
+         AND IS WRITTEN BY AN AMATEUR PYTHON DEVELOPER
 
         =====   THIS COULD GO VERY VERY WRONG!   =====        
         ===== PLEASE BACKUP BEFORE RUNNING THIS! =====
         
-        ARE YOU SURE YOU WANT TO PROCEED? (YES/NO)
+        ARE YOU SURE YOU WANT TO PROCEED? (YES/TEST/NO)
         
- TYPE "YES" IF YOU UNDERSTAND THE RISKS AND WISH TO PROCEED
-*************************************************************
+      TYPE "TEST" TO DO A TEST RUN THAT (HOPEFULLY) WON'T 
+              ACTUALLY DELETE ANY FILES (MAYBE)
+              
+    TYPE "CLEAN" TO MOVE UNWANTED FILES TO A BACKUP DIRECTORY
+                 (/home/pi/RetroPie/cleaned_up)
+        
+    TYPE "DELETE" IF YOU ARE SOUND MIND, UNDERSTAND THE RISKS
+       AND WISH TO PROCEED WITH PERMANENTLY DELETING STUFF!
+*****************************************************************
 : """
 
 user_input = raw_input(question)
@@ -31,7 +39,10 @@ user_input = raw_input(question)
 # be an absolute path as well. Example:
 # walk_dir = os.path.abspath(walk_dir)
 
-if user_input == 'YES' or user_input == 'yes' :
+if user_input == 'DELETE' or user_input == 'delete' or user_input == 'test' or user_input == 'TEST' or user_input == 'clean' or user_input == 'CLEAN' :
+
+    # check if bak_dir exists
+    # if not, make it
 
     for root, subdirs, files in os.walk(rom_dir):
     
@@ -57,11 +68,30 @@ if user_input == 'YES' or user_input == 'yes' :
                     if not os.path.isfile(image_path):
                         ## no image found, we should delete the rom!
                         print "DELETING: " + system_name + "/" + rom_name + " (" + filename + ")"
-                        ## os.remove(file_path)
-                        delete_count += 1
+                        if user_input == 'delete' or user_input == 'DELETE':
+                            ## os.remove(file_path)
+                            delete_count += 1
+                        elif user_input == 'clean' or user_input == 'CLEAN':
+                            ## check if bak_dir/system_name does NOT exist
+                            ## if so, create it
+                            ## then move file to bak_dir/system_name
+                            delete_count += 1
+                        else:
+                            ## do nothing
+                            delete_count += 1
                     
-                    
-    print "\n\n\nCLEANUP COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been deleted! SHINY!"
+    if user_input == 'delete' or user_input == 'DELETE':
+        print "\n--------------------------------------------------------------------"
+        print " CLEANUP COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been deleted! SHINY!"
+        print "--------------------------------------------------------------------"
+    elif user_input == 'clean' or user_input == 'CLEAN':
+        print "\n--------------------------------------------------------------------"
+        print " CLEAN COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been moved! SHINY!"
+        print "--------------------------------------------------------------------"
+    else:
+        print "\n--------------------------------------------------------------------"
+        print " CLEANUP COMPLETE: " + str(delete_count) + " of " + str(total_count) + " are ripe to be tidied!"
+        print "--------------------------------------------------------------------"
     
 else :
     
