a82826dff5975b3bce0bfe5e48cb74891d2a1b26
Andy Warburton <andy@thewarburtons.co.uk>
2016-04-25 23:10:03 +0200

    added cleanup support to remove-imageless-roms.py

--- remove-imageless-roms.py
+++ remove-imageless-roms.py
@@ -5,7 +5,7 @@ import string
 
 # config vars, if you are using non-standard stuff change these
 rom_dir         = '/home/pi/RetroPie/roms'
-bak_dir         = '/home/pi/RetoPie/cleaned_up'
+bak_dir         = '/home/pi/RetroPie/cleaned_up'
 img_dir         = '/opt/retropie/configs/all/emulationstation/downloaded_images'
 allowed_systems = ['amiga','amstradcpc','apple2','arcade','atari800','atari2600','atari5200','atari7800','atarilynx','atarist','c64','coco','dragon32','dreamcast','fba','fds','gamegear','gb','gba','gbc','intellivision','macintosh','mame-advmame','mame-libretro','mame-mame4all','mastersystem','megadrive','msx','n64','neogeo','nes','ngp','ngpc','pc','pcengine','psp','psx','sega32x','segacd','sg-1000','snes','vectrex','videopac','wonderswan','wonderswancolor','zmachine','zxspectrum']
 delete_count    = 0
@@ -34,15 +34,12 @@ question = """
 
 user_input = raw_input(question)
 
-# If your current working directory may change during script execution, it's recommended to
-# immediately convert program arguments to an absolute path. Then the variable root below will
-# be an absolute path as well. Example:
-# walk_dir = os.path.abspath(walk_dir)
-
 if user_input == 'DELETE' or user_input == 'delete' or user_input == 'test' or user_input == 'TEST' or user_input == 'clean' or user_input == 'CLEAN' :
 
-    # check if bak_dir exists
-    # if not, make it
+    # for the cleanup process, we need somewhere to put our backups
+    if user_input == 'clean' or user_input == 'CLEAN':
+        if not os.path.isdir(bak_dir):
+            os.makedirs(bak_dir)
 
     for root, subdirs, files in os.walk(rom_dir):
     
@@ -50,6 +47,8 @@ if user_input == 'DELETE' or user_input == 'delete' or user_input == 'test' or u
 
         with open(list_file_path, 'wb') as list_file:
         
+            os.remove(list_file_path)
+                    
             for filename in files:
                 file_path = os.path.join(root, filename)
             
@@ -63,34 +62,46 @@ if user_input == 'DELETE' or user_input == 'delete' or user_input == 'test' or u
 
                     image_path = img_dir + '/' + system_name + '/' + rom_name + '-image.jpg'
 
-                    #print image_path
-
                     if not os.path.isfile(image_path):
                         ## no image found, we should delete the rom!
-                        print "DELETING: " + system_name + "/" + rom_name + " (" + filename + ")"
+                        
                         if user_input == 'delete' or user_input == 'DELETE':
-                            ## os.remove(file_path)
+
+                            print "DELETING: " + system_name + "/" + rom_name + " (" + filename + ")"
+                            os.remove(file_path)
                             delete_count += 1
+
                         elif user_input == 'clean' or user_input == 'CLEAN':
-                            ## check if bak_dir/system_name does NOT exist
-                            ## if so, create it
-                            ## then move file to bak_dir/system_name
+
+                            print "CLEANING: " + system_name + "/" + rom_name + " (" + filename + ")"
+                            system_bak_dir = bak_dir + '/' + system_name
+                            bak_file_path = system_bak_dir + '/' + filename
+                                                        
+                            if not os.path.isdir(system_bak_dir):
+                                os.makedirs(system_bak_dir)
+
+                            os.rename(file_path, bak_file_path)
                             delete_count += 1
+
                         else:
+
+                            print "TESTING: " + system_name + "/" + rom_name + " (" + filename + ")"
                             ## do nothing
                             delete_count += 1
-                    
+
+    remaining_roms = total_count - delete_count
+    
     if user_input == 'delete' or user_input == 'DELETE':
         print "\n--------------------------------------------------------------------"
-        print " CLEANUP COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been deleted! SHINY!"
+        print "CLEANUP COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been deleted! (" + str(remaining_roms) + " remain)"
         print "--------------------------------------------------------------------"
     elif user_input == 'clean' or user_input == 'CLEAN':
         print "\n--------------------------------------------------------------------"
-        print " CLEAN COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been moved! SHINY!"
+        print "CLEAN COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been moved!  (" + str(remaining_roms) + " remain)"
         print "--------------------------------------------------------------------"
     else:
         print "\n--------------------------------------------------------------------"
-        print " CLEANUP COMPLETE: " + str(delete_count) + " of " + str(total_count) + " are ripe to be tidied!"
+        print "TEST COMPLETE: " + str(delete_count) + " of " + str(total_count) + " are ripe to be tidied!  (" + str(remaining_roms) + " remain)"
         print "--------------------------------------------------------------------"
     
 else :
