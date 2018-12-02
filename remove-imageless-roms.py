e803eca07536fc4071cf20272b845e799739f518
nobodywasishere <m.a.riegert@snet.net>
2018-12-02 00:34:26 -0500

    Flexible home directory, modified output text
    Added the ability for the script to work with RetroPi on Ubuntu as well, by getting the home directory from the OS. 
    Modified the output text to be a bit cleaner and straight forward

--- remove-imageless-roms.py
+++ remove-imageless-roms.py
@@ -1,11 +1,19 @@
+# This program is intended to remove any roms from RetroPie that do not
+# have any artwork. It will work on a Raspberry Pi or a Linux computer
+
+
 import os
 import os.path
 import sys
 import string
 
 # config vars, if you are using non-standard stuff change these
-rom_dir         = '/home/pi/RetroPie/roms'
-bak_dir         = '/home/pi/RetroPie/cleaned_up'
+# os.environ['HOME'] pulls the users current home directory, wherever
+# that is, and changes with the username
+# /opt/ does not need to change as this is standard across RetroPie and
+# linux distros
+rom_dir         = os.environ['HOME'] + '/RetroPie/roms'
+bak_dir         = os.environ['HOME'] + '/RetroPie/cleaned_up'
 img_dir         = '/opt/retropie/configs/all/emulationstation/downloaded_images'
 allowed_systems = [
                   'amiga','amstradcpc','apple2',
@@ -25,75 +33,86 @@ allowed_systems = [
                   'videopac','wonderswan','wonderswancolor',
                   'zmachine','zxspectrum'
                   ]
+
+# The number of deleted roms
 delete_count    = 0
+# The total number of roms
 total_count     = 0
 
-question = """  
+question = """
 *****************************************************************
       WARNING! THIS SCRIPT WILL PERMANENTLY DELETE FILES!
-         AND IS WRITTEN BY AN AMATEUR PYTHON DEVELOPER
 
-        =====   THIS COULD GO VERY VERY WRONG!   =====        
         ===== PLEASE BACKUP BEFORE RUNNING THIS! =====
-        
-        ARE YOU SURE YOU WANT TO PROCEED? (YES/TEST/NO)
-        
-      TYPE "TEST" TO DO A TEST RUN THAT (HOPEFULLY) WON'T 
-              ACTUALLY DELETE ANY FILES (MAYBE)
-              
+      ===== IT IS RECOMMENDED TO DO "TEST" FIRST! =====
+
+      TYPE "TEST" TO DO A TEST RUN THAT WILL TELL YOU HOW
+          MANY FILES WOULD BE CHANGED BY AN ACTUAL RUN
+
     TYPE "CLEAN" TO MOVE UNWANTED FILES TO A BACKUP DIRECTORY
                  (/home/pi/RetroPie/cleaned_up)
-        
-    TYPE "DELETE" IF YOU ARE SOUND MIND, UNDERSTAND THE RISKS
-       AND WISH TO PROCEED WITH PERMANENTLY DELETING STUFF!
+
+              TYPE "DELETE" IF YOU ARE SOUND MIND,
+           UNDERSTAND THE RISKS, AND WISH TO PROCEED
 *****************************************************************
 : """
 
-user_input = raw_input(question).upper()
+spacer = "*****************************************************************"
 
-if user_input in ['DELETE', 'TEST','CLEAN']:
+# question.replace() replaces the output text's CLEAN directory (what is
+# presented to the user) with the actual directory
+user_input = raw_input(question.replace('/home/pi',os.environ['HOME'])).upper()
+
+# verify user input
+if user_input in ['DELETE','TEST','CLEAN']:
 
     # for the cleanup process, we need somewhere to put our backups
     if user_input == 'CLEAN':
         if not os.path.isdir(bak_dir):
             os.makedirs(bak_dir)
 
+    # for all the files and directories to said files in the rom directory
     for root, subdirs, files in os.walk(rom_dir):
-    
+
         list_file_path = os.path.join(root, 'foo.txt')
 
         with open(list_file_path, 'wb') as list_file:
-        
+
             os.remove(list_file_path)
-                    
+
             for filename in files:
                 file_path = os.path.join(root, filename)
-            
-                rom_name = filename.split('.')[0]
-            
-                system_name = file_path.split('/')[-2] 
 
+                rom_name = filename.split('.')[0]
+
+                system_name = file_path.split('/')[-2]
+
+                # verify system is allowed (do not want to mess with
+                # other folders in the roms directory)
                 if system_name in allowed_systems:
 
                     total_count += 1
 
-                    image_path = img_dir + '/' + system_name + '/' + rom_name + '-image.jpg'
+                    image_path = img_dir + '/' + system_name + '/' +
+                                    rom_name + '-image.jpg'
 
                     if not os.path.isfile(image_path):
                         ## no image found, we should delete the rom!
-                        
+
                         if user_input == 'DELETE':
 
-                            print "DELETING: " + system_name + "/" + rom_name + " (" + filename + ")"
+                            print "DELETING: " + system_name + "/" + rom_name +
+                                    " (" + filename + ")"
                             os.remove(file_path)
                             delete_count += 1
 
                         elif user_input == 'CLEAN':
 
-                            print "CLEANING: " + system_name + "/" + rom_name + " (" + filename + ")"
+                            print "CLEANING: " + system_name + "/" + rom_name +
+                                    " (" + filename + ")"
                             system_bak_dir = bak_dir + '/' + system_name
                             bak_file_path = system_bak_dir + '/' + filename
-                                                        
+
                             if not os.path.isdir(system_bak_dir):
                                 os.makedirs(system_bak_dir)
 
@@ -102,25 +121,21 @@ if user_input in ['DELETE', 'TEST','CLEAN']:
 
                         else:
 
-                            print "TESTING: " + system_name + "/" + rom_name + " (" + filename + ")"
+                            print "TESTING: " + system_name + "/" + rom_name +
+                                    " (" + filename + ")"
                             ## do nothing
                             delete_count += 1
 
     remaining_roms = total_count - delete_count
-    
-    if user_input == 'DELETE':
-        print "\n--------------------------------------------------------------------"
-        print "CLEANUP COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been deleted! (" + str(remaining_roms) + " remain)"
-        print "--------------------------------------------------------------------"
-    elif user_input == 'CLEAN':
-        print "\n--------------------------------------------------------------------"
-        print "CLEAN COMPLETE: " + str(delete_count) + " of " + str(total_count) + " files have been moved!  (" + str(remaining_roms) + " remain)"
-        print "--------------------------------------------------------------------"
-    else:
-        print "\n--------------------------------------------------------------------"
-        print "TEST COMPLETE: " + str(delete_count) + " of " + str(total_count) + " are ripe to be tidied!  (" + str(remaining_roms) + " remain)"
-        print "--------------------------------------------------------------------"
-    
-else :
-    
-    print "SCRIPT ABORTED (GOOD CHOICE!)"
+
+    print "\n" + spacer
+    print user_input + " COMPLETE: " + str(delete_count) + " of " +
+        str(total_count) + " are ripe to be tidied!  (" +
+        str(remaining_roms) + " remain)"
+    print spacer
+
+else:
+  
+    print "INPUT NOT RECOGNIZED, PLEASE TRY AGAIN"
+    sys.exit()
+
